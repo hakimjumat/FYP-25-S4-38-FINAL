8404013a16255bdabc23e4d3b72ce0f89580cc86
HumanAnnoyance <94165485+HumanAnnoyance@users.noreply.github.com>
2025-12-11 20:10:54 +0800

    Update HomePage.js

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,11 +1,47 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext, use } from "react";
 import "../CSS/HomePage.css";
-import { auth } from "../firebase";
-import { onAuthStateChanged } from "firebase/auth";
+// import { auth } from "../firebase";
+// import { onAuthStateChanged } from "firebase/auth";
+import { AuthContext } from "../auth/authContext";
+import { authFetch } from "../services/api";
+import { signOut } from "firebase/auth";
+import { auth,db } from "../firebase";
+import { doc, getDoc } from "firebase/firestore";
 
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function NavBar() {
+  const { user } = useContext(AuthContext);
+  const [dropDownOpen, setDropdownOpen] = useState(false);
+  const toggleDropdown = () => {
+    setDropdownOpen(!dropDownOpen);
+  }
+  const navigate = useNavigate();
+  const handleLogOut = async () => {
+    // Perform logout operations here
+    await signOut(auth);
+    navigate("/LoginPage");
+  };
+  useEffect(() => {
+    if (!user) return;
+
+    const fetchUserDetails = async () => {
+      try{
+        const ref = doc(db, "users", user.uid);
+        const snap = await getDoc(ref);
+        if (snap.exists()) {
+          const data = snap.data();
+          user.firstName = data.firstName;
+        }
+        else {
+          console.log("No user data found for UID:", user.uid);
+        }
+      } catch (error) {
+        console.error("Error fetching user details:", error);
+      }
+    };
+    fetchUserDetails();
+  }, [user]);
   return (
     <header className="home-nav">
       <div className="home-nav-left">Learning Platform</div>
@@ -15,9 +51,25 @@ function NavBar() {
         <li>Services</li>
         <li>Contact</li>
       </ul>
-      <Link to="/LoginPage">
-        <button className="home-nav-login-btn">Login / Signup</button>
-      </Link>
+      <div className="home-nav-right">
+        {!user ? (
+          <Link to="/LoginPage">
+            <button className="home-nav-login-btn">Login / Signup</button>
+          </Link>)
+          : (
+            <div className="dropdown">
+              <button className="dropbtn" onClick={toggleDropdown}>
+                {user.firstName} ▼
+              </button>
+              {dropDownOpen && (
+              <div className="dropdown-content">
+                <button onClick={()=> navigate("/ProfilePage")}>Profile</button>  
+                <button onClick={handleLogOut}>Logout</button>
+              </div>
+              )}
+            </div>
+          )}
+      </div>
     </header>
   );
 }
@@ -57,41 +109,64 @@ function Footer() {
 }
 
 function HomePage() {
-  const [user, setUser] = useState(null);
-  const [studentData, setStudentData] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
+  // const [user, setUser] = useState(null);
+  // const [studentData, setStudentData] = useState(null);
+  // const [loading, setLoading] = useState(false);
+  // const [error, setError] = useState(null);
+  const { user } = useContext(AuthContext);
+  const [profile, setProfile] = useState(null);
+  const [gamification, setGamification] = useState(null);
+  // useEffect(() => {
+  //   console.log("Entering Homepage");
+  //   const unsubscribe = onAuthStateChanged(auth, async (firebaseUser) => {
+  //     setUser(firebaseUser || null);
+
+  //     if (firebaseUser) {
+  //       try {
+  //         const token = await firebaseUser.getIdToken();
+
+  //         const response = await fetch(
+  //           "http://localhost:5000/api/students/profile",
+  //           {
+  //             method: "GET",
+  //             headers: {
+  //               "Content-Type": "application/json",
+  //               Authorization: `Bearer ${token}`,
+  //             },
+  //           }
+  //         );
+
+  //         const data = await response.json();
+  //         setStudentData(data);
+  //       } catch (error) {
+  //         console.error("Error fetching student data:", error);
+  //       }
+  //     }
+  //   });
+
+  //   return () => unsubscribe();
+  // }, []);
 
   useEffect(() => {
-    console.log("Entering Homepage");
-    const unsubscribe = onAuthStateChanged(auth, async (firebaseUser) => {
-      setUser(firebaseUser || null);
+    if (!user) return;
 
-      if (firebaseUser) {
-        try {
-          const token = await firebaseUser.getIdToken();
 
-          const response = await fetch(
-            "http://localhost:5000/api/students/profile",
-            {
-              method: "GET",
-              headers: {
-                "Content-Type": "application/json",
-                Authorization: `Bearer ${token}`,
-              },
-            }
-          );
+    (async () => {
+    try {
+    const result = await authFetch("http://localhost:5000/api/auth/current-user", {}, user);
+    if (result.success) {
+    setProfile(result.data.profile);
+    setGamification(result.data.gamification);
+    }
+    } catch (err) {
+    console.error(err);
+    }
+    })();
+    }, [user]);
 
-          const data = await response.json();
-          setStudentData(data);
-        } catch (error) {
-          console.error("Error fetching student data:", error);
-        }
-      }
-    });
 
-    return () => unsubscribe();
-  }, []);
+    if (!user) return <div>Please login</div>;
+
 
   return (
     <div className="home-page">
@@ -104,16 +179,20 @@ function HomePage() {
           </p>
 
           {user && (
-            <p className="home-logged-in-text">
-              You are currently logged in as <strong>{user.email}</strong>.
+            <div className="home-welcome-box">
+              <button className="profilePage"><Link to="/ProfilePage">Go to Profile</Link></button>
+              <p className="home-logged-in-text">
+              You are currently logged in as <strong>{profile?.name ?? user.email}</strong>.
             </p>
+            <pre>{JSON.stringify({ profile, gamification}, null, 2)}</pre>
+            </div>
           )}
 
-          {loading && <p>Loading your profile...</p>}
-          {error && <p style={{ color: "red" }}>⚠️ Error: {error}</p>}
+          {/* {loading && <p>Loading your profile...</p>}
+          {error && <p style={{ color: "red" }}>⚠️ Error: {error}</p>} */}
 
           {/* DISPLAY BACKEND DATA */}
-          {studentData && studentData.gamification && (
+          {/* {studentData && studentData.gamification && (
             <div className="home-progress-box">
               <h3>🎓 Your Progress</h3>
               <p>
@@ -133,7 +212,7 @@ function HomePage() {
                   : "No badges yet - keep learning!"}
               </p>
             </div>
-          )}
+          )} */}
         </section>
       </main>
     </div>
